4540d41dce974da8fac8c0c0832220dd77de5a67
Guan-Yu Chen <r06246010@ntu.edu.tw>
2021-01-05 22:05:38 +0800

    Rewrite Ex2_01 by generic function max(type)

--- Chapter 2/01_ranges_of_variable_types.c
+++ Chapter 2/01_ranges_of_variable_types.c
@@ -9,6 +9,97 @@
 #include <limits.h>
 #include <float.h>
 
+#define max(X) _Generic((X), \
+                        char: max_char, \
+                        signed char: max_char, \
+                        short: max_short, \
+                        int: max_int, \
+                        long: max_long, \
+                        long long: max_llong, \
+                        unsigned char: max_uchar, \
+                        unsigned short: max_ushort, \
+                        unsigned int: max_uint, \
+                        unsigned long: max_ulong, \
+                        unsigned long long: max_ullong \
+                        )(X)
+
+char max_char(char max){
+    for (char i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
+signed char max_schar(signed char max){
+    for (signed char i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
+short max_short(short max){
+    for (short i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
+int max_int(int max){
+    for (int i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
+long max_long(long max){
+    for (long i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
+long long max_llong(long long max){
+    for (long long i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
+unsigned char max_uchar(unsigned char max){
+    for (unsigned char i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
+unsigned short max_ushort(unsigned short max){
+    for (unsigned short i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
+unsigned int max_uint(unsigned int max){
+    for (unsigned int i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
+unsigned long max_ulong(unsigned long max){
+    for (unsigned long i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
+unsigned long long max_ullong(unsigned long long max){
+    for (unsigned long long i = 1; i > max; i <<= 1, ++i) {
+        max = i;
+    }
+    return max;
+}
+
 int main(int argc, const char *argv[])
 {
     printf("From Standard Headers\n");
@@ -36,53 +127,31 @@ int main(int argc, const char *argv[])
     unsigned long long max;
     printf("Direct Computation Test\n");
     printf("-------------------------\n");
-    for (char i = 0; i >= 0; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((char)0);
     printf("Range of 'char': %lld to %lld\n", -max-1, max);
-    for (signed char i = 0; i >= 0; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((signed char)0);
     printf("Range of 'signed char': %lld to %lld\n", -max-1, max);
-    for (unsigned char i = 1; i != max; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((unsigned char)0);
     printf("Range of 'unsigned char': %d to %llu\n\n", 0, max);
 
-    for (short i = 0; i >= 0; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((short)0);
     printf("Range of 'short': %lld to %lld\n", -max-1, max);
-    for (unsigned short i = 1; i != max; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((unsigned short)0);
     printf("Range of 'unsigned short': %d to %llu\n\n", 0, max);
 
-    for (int i = 0; i >= 0; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((int)0);
     printf("Range of 'int': %lld to %lld\n", -max-1, max);
-    for (unsigned int i = 1; i != max; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((unsigned int)0);
     printf("Range of 'unsigned int': %d to %llu\n\n", 0, max);
 
-    for (long i = 0; i >= 0; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((long)0);
     printf("Range of 'long': %lld to %lld\n", -max-1, max);
-    for (unsigned long i = 1; i != max; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((unsigned long)0);
     printf("Range of 'unsigned long': %d to %llu\n\n", 0, max);
 
-    for (long long i = 0; i >= 0; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((long long)0);
     printf("Range of 'long long': %lld to %lld\n", -max-1, max);
-    for (unsigned long long i = 1; i != max; i <<= 1, ++i) {
-        max = i;
-    }
+    max = max((unsigned long long)0);
     printf("Range of 'unsigned long long': %d to %llu\n\n", 0, max);
     return 0;
 }
